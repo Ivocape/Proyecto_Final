1ecb5e5503d5de5697f93a493849a49da187cd2f
dustinhellmann <dhellmann@itba.edu.ar>
2023-11-29 16:11:01 -0300

    asas

---
+++ discoDuro.py
@@ -0,0 +1,41 @@
+import csv
+import pandas as pd
+import datetime
+#####################################################################################
+# ESTE DISCO DURO DEBE DE ENCARGARSE DE GUARDAR TODA INFORMACION DE USUARIOS, CLIENTES, PERSONAL, ADMINS,
+#####################################################################################
+class DiscoDuro():
+    # Crear el disco duro
+    def __init__(self) -> None:
+        pass
+
+    def leerSETUP (self, carpeta):    
+        # Abrir el archivo CSV
+        with open(carpeta,'r', newline='') as csvfile: 
+            # Crear un objeto lector CSV
+            reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+            match carpeta:
+                case "proyectos_2015.csv":
+                            
+                    for row in reader:
+                        # Extraigo la informacion de cada fila de los proyectos del 2015
+                        proyecto_id = row[0]
+                        fuente = row[1]
+                        titulo = row[2]
+                        fecha_inicio = row[3]
+                        fecha_fin = row[4]
+                        resumen = row[5]
+                        moneda_id = row[6]
+                        monto_total_solicitado = row[7]
+                        monto_total_adjudicado = row[8]
+                        monto_financiado_solicitado= row[9]
+                        monto_financiado_adjudicado = row[10]
+                        tipo_proyecto_id = row[11]
+                        codigo_identificacion= row[12]
+                        palabras_clave = row[13]
+                        estado_id = row[14]
+                        fondo_anpcyt= row[15]
+                        cantidad_miembros_F = row[16]
+                        cantidad_miembros_M = row[17]
+                        sexo_director = row[18]
+                        
---
+++ procesos.py
@@ -0,0 +1,18 @@
+#Mostrar la distribución de los proyectos por área de investigación (por ejemplo, ciencias sociales,ciencias naturales, tecnología, etc.) y sus correspondientes sub áreas ( por ejemplo de cienciasnaturales las sub áreas de matemáticas, ciencias de la computación, ciencias físicas....)
+class Cache:
+    def __init__(self):
+        self.2015={}
+        self.2016={}
+        self.2017={}
+        self.2018={}
+        self.ref_disciplina={}
+        self.ref_estado_proyecto={}
+        self.ref_funcion={}
+        self.ref_moneda={}
+        self.ref_tipo_proyecto={}
+        self.proyecto_disciplina={}
+        self.proyecto_participante={}
+        
+
+        
+        
