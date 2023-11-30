ddf1ebc67da58a382a48cacfb01b1b79e36fee34
Ivocape <145204318+Ivocape@users.noreply.github.com>
2023-11-30 11:07:46 -0300

    ..

--- GUI.py
+++ GUI.py
@@ -3,14 +3,15 @@ from tkinter import Tk, Frame, Canvas, Button
 from procesos import *
 from discoDuro import *
 from menus import *
+from histogramas import *
 from tkinter import *
   
   
 class HistogramGUI:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
         self.discoDuro=DiscoDuro()
         self.cache=Cache()
+        self.histogramas=histogramas()
     
         self.root = Tk()
         self.root.title("Histograma")
@@ -21,27 +22,17 @@ class HistogramGUI:
         self.canvas = Canvas(self.frame, width=400, height=300)
         self.canvas.pack()
 
-        self.button = Button(self.frame, text="Mostrar Histograma", command=self.show_histogram)
+        self.button = Button(self.frame, text="Mostrar Histograma", command=self.histogramas.show_histogram)
         self.button.pack()
     def setup(self):
 
         listacsv=['users.csv', 'reservas.csv', 'room.csv','buffet.csv', 'tareas.csv', 'pedidos.csv','ingresos.csv','inversion.csv']
         for carpeta in listacsv:
             self.discoDuro.leerSETUP(carpeta)
-            
-    def show_histogram(self):
-        plt.hist(self.data)
-        plt.xlabel("Valores")
-        plt.ylabel("Frecuencia")
-        plt.title("Histograma")
-        plt.show()
-
+        
     def run(self):
         self.root.mainloop()
-
-# Ejemplo de uso
-data = [1, 2, 3, 4, 5, 5, 5, 6, 6, 7, 8, 9, 10]
             
-instance=HistogramGUI(data)
-instance.setup()
+instance=HistogramGUI()
+#instance.setup()
 instance.run()
---
+++ histogramas.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+from procesos import *
+class histogramas():
+    def __init__(self):
+      pass
+    def show_histogram(self, data):
+        plt.hist()
+        plt.xlabel("Valores")
+        plt.ylabel("Frecuencia")
+        plt.title("Histograma")
+        plt.show()
