54658d4b62c4958b9bcb7db8e134ab06940f1854
Ivocape <145204318+Ivocape@users.noreply.github.com>
2023-11-30 12:05:26 -0300

    .

--- GUI.py
+++ GUI.py
@@ -16,7 +16,7 @@ class HistogramGUI:
         self.analisis=Analisis()
     
         self.root = Tk()
-        self.root.title("Histograma")
+        self.root.title("Menu Principal")
         
         self.frame = Frame(self.root)
         self.frame.pack()
@@ -24,11 +24,27 @@ class HistogramGUI:
         self.canvas = Canvas(self.frame, width=400, height=300)
         self.canvas.pack()
 
-        self.button = Button(self.frame, text="Mostrar Histograma", command=self.histogramas.show_histogram)
+        self.button = Button(self.frame, text="distribución de los proyectos por área de investigación", command=self.histogramas.show_histogram)
         self.button.pack()
+
+        self.button = Button(self.frame, text="porcentaje de participación de las mujeres en los diferentes proyectos según el rol quedesempeña", command=self.histogramas.show_histogram)
+        self.button.pack()
+
+        self.button = Button(self.frame, text="tiempo promedio de terminación", command=self.histogramas.show_histogram)
+        self.button.pack()
+
+        self.button = Button(self.frame, text=" porcentaje de proyectos que han utilizado tecnologías emergentes", command=self.histogramas.show_histogram)
+        self.button.pack()
+
+        self.button = Button(self.frame, text="lista de proyectos", command=self.histogramas.show_histogram)
+        self.button.pack()
+
+        self.button = Button(self.frame, text="Financiamiento Solicitado/Otorgado", command=self.histogramas.show_histogram)
+        self.button.pack()
+
     def setup(self):
 
-        listacsv=['users.csv', 'reservas.csv', 'room.csv','buffet.csv', 'tareas.csv', 'pedidos.csv','ingresos.csv','inversion.csv']
+        listacsv=['proyectos_2015.csv','proyectos_2016.csv','proyectos_2017.csv','proyectos_2018.csv']
         for carpeta in listacsv:
             self.discoDuro.leerSETUP(carpeta)
         
