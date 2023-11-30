e646a632893ebc48cf1eca9a24f7637eaa735872
dustinhellmann <dhellmann@itba.edu.ar>
2023-11-30 15:13:55 -0300

    s

--- GUI.py
+++ GUI.py
@@ -16,7 +16,7 @@ class HistogramGUI:
         self.analisis=Analisis()
     
         self.root = Tk()
-        self.root.title("Menu Principal")
+        self.root.title("Histograma")
         
         self.frame = Frame(self.root)
         self.frame.pack()
@@ -24,24 +24,8 @@ class HistogramGUI:
         self.canvas = Canvas(self.frame, width=400, height=300)
         self.canvas.pack()
 
-        self.button = Button(self.frame, text="distribución de los proyectos por área de investigación", command=self.histogramas.show_histogram)
+        self.button = Button(self.frame, text="Mostrar Histograma", command=self.histogramas.show_histogram)
         self.button.pack()
-
-        self.button = Button(self.frame, text="porcentaje de participación de las mujeres en los diferentes proyectos según el rol quedesempeña", command=self.histogramas.show_histogram)
-        self.button.pack()
-
-        self.button = Button(self.frame, text="tiempo promedio de terminación", command=self.histogramas.show_histogram)
-        self.button.pack()
-
-        self.button = Button(self.frame, text=" porcentaje de proyectos que han utilizado tecnologías emergentes", command=self.histogramas.show_histogram)
-        self.button.pack()
-
-        self.button = Button(self.frame, text="lista de proyectos", command=self.histogramas.show_histogram)
-        self.button.pack()
-
-        self.button = Button(self.frame, text="Financiamiento Solicitado/Otorgado", command=self.histogramas.show_histogram)
-        self.button.pack()
-
     def setup(self):
 
         listacsv=['proyectos_2015.csv','proyectos_2016.csv','proyectos_2017.csv','proyectos_2018.csv','proyecto_disciplina.csv','proyecto_participante.csv','ref_disciplina.csv','ref_estado_proyecto.csv','ref_funcion.csv','ref_moneda.csv','ref_tipo_proyecto.csv']
