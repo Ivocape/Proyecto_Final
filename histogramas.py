02d09718be3dcc8b3e44db2b37ed29d4ac04c0f4
tcalderonnn <147106615+tcalderonnn@users.noreply.github.com>
2023-12-05 15:42:45 -0300

    aa

--- histogramas.py
+++ histogramas.py
@@ -14,11 +14,11 @@ class histogramas:
   def mostrar_popup(self, mensaje_principal, datos=None):
     nuevaventana = tk.Tk()
     nuevaventana.title("Popup")
-    nuevaventana.geometry("800x200")
+    nuevaventana.geometry("700x200")
     nuevaventana.configure(bg="lightblue")
 
     #crear una etiqueta con el mensaje principal
-    label_principal = tk.Label(nuevaventana, text=mensaje_principal, bg="white", fg="black", justify="left", font=("Arial", 10))
+    label_principal = tk.Label(nuevaventana, text=mensaje_principal, bg="white", fg="black", justify="left", font=("Arial", 20))
     label_principal.pack()
 
     if datos is not None:
